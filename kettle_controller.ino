b6d60e2b09e5c8f9dc307aa92a131c2d612d2d4e
centaur16 <david.misc1@aspect135.co.uk>
2020-02-29 20:35:56 +0000

    Added support for ping and IR ranging sensors, and serial logging for RFID presence

--- kettle_controller.ino
+++ kettle_controller.ino
@@ -9,6 +9,11 @@
 
 #define LED_PIN 3
 
+#define PING_TRIG_PIN A1
+#define PING_ECHO_PIN A0
+
+#define IR_D_PIN A3
+#define IR_A_PIN A2
 
 MFRC522 rfid_sens(SS_PIN, RST_PIN);
 Adafruit_BME280 TPH_sens;
@@ -16,13 +21,45 @@ float temp_c, pressure_pa, humidity_percent;
 Adafruit_TCS34725 RGB_sens;
 uint16_t red, green, blue, clear_light;
 
+float ping_dist_cm;
+uint16_t ir_proximity;
+
 uint32_t count;
+
+bool card_present; 
 
 bool is_card_present(MFRC522 tag)
 {
   bool test = tag.PICC_IsNewCardPresent();
   test = (tag.PICC_IsNewCardPresent() || test);
   return test;
+}
+
+float ping_get_distance_cm()
+{
+  long echo_time;
+  float distance_cm;
+  digitalWrite(PING_TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PING_TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PING_TRIG_PIN, LOW);
+
+  echo_time = pulseIn(PING_ECHO_PIN, HIGH, 10000);
+  distance_cm = (echo_time * 0.0343) / 2.0;
+  return distance_cm;
+}
+
+uint16_t ir_get_proximity()
+{
+  if (digitalRead(IR_D_PIN) == HIGH)
+  {
+    return analogRead(IR_A_PIN);
+  }
+  else
+  {
+    return 0;
+  }
 }
 
 void setup() {
@@ -36,6 +73,9 @@ void setup() {
 
   TBH_status = TPH_sens.begin();
   RGB_status = RGB_sens.begin();
+
+  pinMode(PING_TRIG_PIN, OUTPUT);
+  pinMode(PING_ECHO_PIN, INPUT);
 }
 
 void loop() {
@@ -47,13 +87,19 @@ void loop() {
 
   if (is_card_present(rfid_sens))
   {
+    card_present = true;
     Serial.println("Card present..");
     digitalWrite(LED_PIN, HIGH);
   }
   else
   {
+    card_present = false;
     digitalWrite(LED_PIN, LOW);
   }
+
+  ping_dist_cm = ping_get_distance_cm();
+  ir_proximity = ir_get_proximity();
+
   Serial.print(count);
   Serial.print(" - ");
   Serial.print(temp_c);
@@ -69,6 +115,12 @@ void loop() {
   Serial.print(blue);
   Serial.print(", C: ");
   Serial.print(clear_light);
+  Serial.print(", RFID: ");
+  Serial.print(card_present);
+  Serial.print(", PING: ");
+  Serial.print(ping_dist_cm);
+  Serial.print(", IR: ");
+  Serial.print(ir_proximity);
   Serial.println("");
   count++;
   delay(50);
